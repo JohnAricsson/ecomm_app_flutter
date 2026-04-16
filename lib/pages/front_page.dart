3afd7c980cf2e9693c930d8be16e0d86882e0b86
John Aricsson Majumder <johnaricsson1@gmail.com>
2026-04-16 18:28:06 +0600

    Initialize flutter project

---
+++ lib/pages/front_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FrontPage extends StatelessWidget {
+  const FrontPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(children:[
+          
+        ]
+
+      ),
+    );
+  }
+}
